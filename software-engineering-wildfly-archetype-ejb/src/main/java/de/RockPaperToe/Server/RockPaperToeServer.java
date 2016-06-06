e15781ca5c154da71dcf29121165e6bbf7a8f34f
KevinThielen <genusnymphicus@gmail.com>
2016-06-06 10:24:02 +0200

    removed last change

--- software-engineering-wildfly-archetype-ejb/src/main/java/de/RockPaperToe/Server/RockPaperToeServer.java
+++ software-engineering-wildfly-archetype-ejb/src/main/java/de/RockPaperToe/Server/RockPaperToeServer.java
@@ -3,13 +3,10 @@ package de.RockPaperToe.Server;
 import javax.ejb.Stateless;
 import javax.jws.WebService;
 
-
-import org.jboss.ws.api.annotation.WebContext;
 /**
  * Session Bean implementation class RockPaperToeServer
  */
 @WebService
-@WebContext(contextRoot = "/UnoServer")
 @Stateless
 public class RockPaperToeServer {
 
