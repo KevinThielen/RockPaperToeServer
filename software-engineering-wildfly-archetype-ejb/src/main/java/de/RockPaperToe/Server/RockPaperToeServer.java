9ef54085c9329b5e24b148733aaad7bebfa11ba3
KevinThielen <genusnymphicus@gmail.com>
2016-06-06 10:10:10 +0200

    simple configuration test

---
+++ software-engineering-wildfly-archetype-ejb/src/main/java/de/RockPaperToe/Server/RockPaperToeServer.java
@@ -0,0 +1,18 @@
+package de.RockPaperToe.Server;
+
+import javax.ejb.Stateless;
+import javax.jws.WebService;
+
+
+import org.jboss.ws.api.annotation.WebContext;
+/**
+ * Session Bean implementation class RockPaperToeServer
+ */
+@WebService
+@WebContext(contextRoot = "/UnoServer")
+@Stateless
+public class RockPaperToeServer {
+
+ 
+
+}
